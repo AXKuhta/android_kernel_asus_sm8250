108bcdd82d18535dc1abe520c1be68c04adff53a
Anon <No email>
2021-04-16 15:25:17 +0800

    Enable ramoops in the defconfigs

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -511,6 +511,9 @@ static void msm_restart_prepare(const char *cmd)
 		// Normal reboot. Clean the printk buffer magic
 		printk_buffer_slot2_addr = (ulong *)PRINTK_BUFFER_SLOT2;
 		*printk_buffer_slot2_addr = 0;
+	} else {
+		// Panic reboot
+		need_warm_reset = true;
 	}
 
 	if (force_warm_reboot)
